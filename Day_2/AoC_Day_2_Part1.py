035d12ac7cbc474f8816d8fb2b53e7688b6891d1
Lance Vincent Sebastian <76014401+LanceSebastian@users.noreply.github.com>
2023-12-02 20:36:19 +0000

    include processGame() method
    This method goes through the number of cubes picked and checks if the number of cubes doesn't break the rules

--- Day_2/AoC_Day_2_Part1.py
+++ Day_2/AoC_Day_2_Part1.py
@@ -1,5 +1,11 @@
 #Advent of Code
 #Day 2: Cube Conundrum
+def processGame(game):
+    for cube in game:
+        if not isLegal(cube[0],cube[1]):
+            return False
+
+
 def processLine(line):
     legal = True
     game_id = getGameID(line)
