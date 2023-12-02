1906d54630406bfe96be12b66154bb3638c37a36
Lance Vincent Sebastian <76014401+LanceSebastian@users.noreply.github.com>
2023-12-02 20:32:23 +0000

    include readFile() method

--- Day_2/AoC_Day_2_Part1.py
+++ Day_2/AoC_Day_2_Part1.py
@@ -1,5 +1,12 @@
 #Advent of Code
 #Day 2: Cube Conundrum
+def readFile():
+    sum = 0
+    with open("sample.txt") as file:
+        for line in file:
+            sum += processLine(line)
+    return sum
+
 
 def main():
     print("Hello World")
