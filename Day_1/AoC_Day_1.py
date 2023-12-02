bfd7d7467d1fed5a6f3b9d9e6a8601bde04a5095
Lance Vincent Sebastian <lvsebastian2002@gmail.com>
2023-12-02 02:10:56 +0000

    solve day1 part1 of AoC

---
+++ Day_1/AoC_Day_1.py
@@ -0,0 +1,28 @@
+#Advent of Code
+#Day 1: Trebuchet?!
+
+def getNumbers(line):
+    numbers = ""
+    for character in line:
+        if character.isdigit():
+            numbers += character
+    return numbers
+
+
+def processLine(line):
+    string_numbers = getNumbers(line)
+    final_number = string_numbers[0] + string_numbers[-1]
+    return int(final_number)
+
+def readFile():
+    total = 0
+    with open("F:\Lance's Stuff\GitHub\Advent-of-Code-2023\Day_1\sample.txt") as file:
+        for line in file:
+            total += processLine(line)
+    return total
+
+def main():
+    print(readFile())
+
+if __name__ == '__main__':
+    main()
