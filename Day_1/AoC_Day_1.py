2590e78503ab484331f9960afbcce060372d32c6
Lance Vincent Sebastian <lvsebastian2002@gmail.com>
2023-12-02 02:53:19 +0000

    Update AoC_Day_1.py

--- Day_1/AoC_Day_1.py
+++ Day_1/AoC_Day_1.py
@@ -8,7 +8,6 @@ def getNumbers(line):
             numbers += character
     return numbers
 
-
 def processLine(line):
     string_numbers = getNumbers(line)
     final_number = string_numbers[0] + string_numbers[-1]
@@ -16,7 +15,7 @@ def processLine(line):
 
 def readFile():
     total = 0
-    with open("F:\Lance's Stuff\GitHub\Advent-of-Code-2023\Day_1\sample.txt") as file:
+    with open("F:\Lance's Stuff\GitHub\Advent-of-Code-2023\Day_1\Puzzle_Input.txt") as file:
         for line in file:
             total += processLine(line)
     return total
